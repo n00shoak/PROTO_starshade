222179d7a74c77aa337d30b7ed0e34b6200af92a
n00shoak <118372243+n00shoak@users.noreply.github.com>
2024-04-09 12:10:59 +0200

    back up

--- Assets/IA/IA Tasks Manager/CL_Task.cs
+++ Assets/IA/IA Tasks Manager/CL_Task.cs
@@ -7,8 +7,8 @@ using UnityEngine.Events;
 public class CL_Task : MonoBehaviour
 {
     // General Data
-    public DT_TaskTypes.tasksType type;
-    public float inatePriority; // 0 = low priority   10 = high priority
+    private DT_TaskTypes.tasksType type;
+    private float inatePriority; // 0 = low priority   10 = high priority
 
     // Speceific data
     public float whichStep;
